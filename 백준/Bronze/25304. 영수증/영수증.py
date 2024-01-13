07465549736611c2e2c7ef6240a408674b9c35c2
MooYoo <82500842+yasisicoco@users.noreply.github.com>
2024-01-13 14:54:15 +0900

    [Bronze IV] Title: 영수증, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.py
@@ -0,0 +1,11 @@
+X = int(input())
+N = int(input())
+
+sum = 0
+for i in range(1, N + 1):
+    a, b = map(int, input().split())
+    sum = sum + (a * b)
+if X == sum:
+    print("Yes")
+else:
+    print("No")
