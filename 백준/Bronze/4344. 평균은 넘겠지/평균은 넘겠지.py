167f9b90e851e9e34a1253b91d260145717e5935
MooYoo <82500842+yasisicoco@users.noreply.github.com>
2024-01-26 00:44:45 +0900

    Update 평균은 넘겠지.py
    주석 추가

--- 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
@@ -1,16 +1,16 @@
 C = int(input())
 
 for i in range(C):
-    num = list(map(int, input().split()))
-    numK = num.pop(0)
-    number = 0
-    aver_num = 0
+    num = list(map(int, input().split())) # 점수갯수와 점수값 받기
+    numK = num.pop(0) # numK 에 점수갯수 반환
+    number = 0 # 총점수 저장 변수
+    aver_num = 0 # 평균점수를 넘는 점수갯수 저장 변수
     for j in num:
-        number += j
-    sum_number = number / numK
+        number += j # 총점수 계산
+    sum_number = number / numK # sum_number에 평균값 계산
     for k in num:
-        if k > sum_number:
-            aver_num += 1
+        if k > sum_number: # 평균값보다 높은 점수 구분
+            aver_num += 1 # aver_num에 평균값보다 높은 점수 갯수 저장
     
-    result = aver_num / numK * 100
-    print('{0:.3f}%'.format(result))
+    result = aver_num / numK * 100 # 평균값보다 높은점수갯수 / 원래 점수갯수
+    print('{0:.3f}%'.format(result)) # 포맷함수를 통해 소수점 셋째자리까지 표현
