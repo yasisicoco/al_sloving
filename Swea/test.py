cac57c145367d3ed43835c7d07a6f9a91ef8dd88
jinwoo <yasisicoco@gmail.com>
2024-02-06 02:29:20 +0900

    mario play

--- Swea/test.py
+++ Swea/test.py
@@ -60,29 +60,78 @@
 
 # delta_array 연습문제 1-2
 # 배열 만들기
-N = 5
-arr = [[0] * N for _ in range(N)]
-num = 1
+# N = 5
+# arr = [[0] * N for _ in range(N)]
+# num = 1
 
-for r in range(0, N):
-    for c in range(0, N):
-        arr[r][c] = num
-        num += 1
-for line in arr:
-    print(*line)
+# for r in range(0, N):
+    # for c in range(0, N):
+        # arr[r][c] = num
+        # num += 1
+# for line in arr:
+    # print(*line)
 
 # 델타인덱스 이용하여 주변수 더하기 (요소 총 25개)
-dr = [0, 1, 0, -1]
-dc = [1, 0, -1, 0]
-result = 0
-for r in range(N):
-    for c in range(N):
-        total_sum = arr[r][c]
-        for k in range(4):  # 0, 1, 2, 3 #조작키 범위 3부터 시계방향
-            nr = r + dr[k]
-            nc = c + dc[k]
-            if 0 <= nr < N and 0 <= nc < N: #인덱스벨류 오류 피하기
-                total_sum += arr[nr][nc]
-                result += total_sum
+# dr = [0, 1, 0, -1]
+# dc = [1, 0, -1, 0]
+# result = 0
+# for r in range(N):
+    # for c in range(N):
+        # total_sum = arr[r][c]
+        # for k in range(4):  # 0, 1, 2, 3 #조작키 범위 3부터 시계방향
+            # nr = r + dr[k]
+            # nc = c + dc[k]
+            # if 0 <= nr < N and 0 <= nc < N: #인덱스벨류 오류 피하기
+                # total_sum += arr[nr][nc]
+                # result += total_sum
 
-print(result)
+# print(result)
+
+
+# 14425
+# N, M = map(int, input().split())
+# S = []
+# for _ in range(N): # N 번의 반복
+    # str1 = input() # 동안 문자열 한개씩
+    # S.append(str1) # 집합 S에 추가
+
+# cnt = 0 # 집합 S에 포함된 문자열의 갯수
+# for _ in range(M):
+    # str2 = input()
+    # if str2 in S:
+        # cnt += 1
+# print(cnt)
+
+# 2738
+# N, M = map(int, input().split())
+# A = [list(map(int, input().split())) for _ in range(N)]
+# B = [list(map(int, input().split())) for _ in range(N)]
+
+# for i in range(N):
+    # for j in range(M):
+        # A[i][j] += B[i][j]
+
+# for line in A:
+    # print(*line)
+
+
+# 2851
+
+lst = []
+for tc in range(10):
+    N = int(input())
+    lst.append(N)
+
+max_val = 0
+num = len(lst)
+for i in range(0, num):
+    max_val += lst[i]
+    if max_val > 100:
+        real_i = i
+        break
+realmax = max_val - 100
+fakemax = 100 - (max_val - lst[real_i])
+if abs(100 - realmax) <= abs(100 - fakemax):
+    print(max_val)
+else:
+    print(max_val - lst[real_i])
--- TEST.py
+++ TEST.py
@@ -1,24 +1,39 @@
 # N :  배열 A의 크기, K : 교환 횟수
-N, K = map(int, input().split())
-a = list(map(int, input().split()))
+# N, K = map(int, input().split())
+# a = list(map(int, input().split()))
+# 
+# def bubble_sort(a, N, K):
+    # cnt = 0
+    # res = []
+    # for last in range(N-1, 0, -1):
+        # if cnt == K:
+            # res.append(a[i])
+            # res.append(a[i + 1])
+            # break
+        # for i in range(last):
+            # if a[i] > a[i+1]:
+                # a[i], a[i+1] = a[i+1], a[i]
+                # cnt += 1
+    # if len(res) == 0:
+        # res.append(-1)
+    # return res
+# 
+# result = bubble_sort(a, N, K)
+# 
+# for r in range(len(result)):
+    # print(result[r], end=' ')
 
-def bubble_sort(a, N, K):
-    cnt = 0
-    res = []
-    for last in range(N-1, 0, -1):
-        if cnt == K:
-            res.append(a[i])
-            res.append(a[i + 1])
-            break
-        for i in range(last):
-            if a[i] > a[i+1]:
-                a[i], a[i+1] = a[i+1], a[i]
-                cnt += 1
-    if len(res) == 0:
-        res.append(-1)
-    return res
 
-result = bubble_sort(a, N, K)
 
-for r in range(len(result)):
-    print(result[r], end=' ')
+
+# 28445. 알록달록 앵무새
+color_1, color_2 = input().split()
+color_3, color_4 = input().split()
+
+s = set([color_1, color_2, color_3, color_4])
+lst = list(s)
+lst.sort()
+
+for i in lst:
+    for j in lst:
+        print(i, j)
