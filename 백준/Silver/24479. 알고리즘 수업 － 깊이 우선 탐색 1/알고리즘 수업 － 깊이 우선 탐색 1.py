21b987d56aa621e9e721f96902329edbcbe8917b
MooYoo <82500842+yasisicoco@users.noreply.github.com>
2024-02-12 00:44:28 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 628 ms, Memory: 68780 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.py
@@ -0,0 +1,40 @@
+import sys
+sys.setrecursionlimit(10 ** 6)
+
+input = sys.stdin.readline
+
+N, M, R = map(int, input().split())
+    
+# 인접리스트
+v = [[] for _ in range(N+1)]
+# 방문 처리 리스트
+visited = [False for _ in range(N+1)]
+
+for i in range(M):
+    a, b = map(int, input().split())
+    v[a].append(b)
+    v[b].append(a)
+    
+for j in v:
+    j.sort()
+    
+vis = [0 for _ in range(N+1)]
+cnt = 1
+def dfs(cur):
+    global cnt
+    
+    for i in v[cur]:
+        if visited[i] == True:
+            continue
+        visited[i] = True
+        cur = i
+        cnt += 1
+        vis[i] = cnt
+        dfs(cur)
+        
+vis[R] = 1
+visited[R] = True
+dfs(R)
+
+for k in range(1, N+1):
+    print(vis[k])
