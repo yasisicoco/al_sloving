8550b61cb872ac6d230f137ca81d989800711adb
jinwoo <yasisicoco@gmail.com>
2024-02-22 23:19:10 +0900

    omok

--- Swea Test.py
+++ Swea Test.py
@@ -1,32 +1,45 @@
-arr = '0A7C'
-#     '0000 1010 0111 1101'
-#     [0, 0, 0, 0, 1, 0, 1, 0, ...]
-#16진수 문자열 -> 2진수 문자열(리스트)
-hex_dict = {
-    '0': '0000', '7': '0111', 'A': '1010', 'C': '1100'
-}
-bin_str = ''
-for val in arr:
-    bin_str += hex_dict[val]
+# 11315. 오목
 
-print(bin_str)
-# ---------------------------------------
-hex_dict = {
-    '0': [0,0,0,0], '7': [0,1,1,1], 'A': [1,0,1,0], 'C': [1,1,0,0]
-}
-bin_str = []
-for val in arr:
-    # bin_str += hex_dict[val]
-    bin_str.extend(hex_dict[val])
-print(bin_str)
+# 구현으로 풀어보자
+# 주어진 정수 N 은 오목판의 크기 (가로x세로)
+# 문자열로 배열 만들기
+# 포문 두개로 배열 돌면서 o 찾기
+# 방향 벡터는 오른쪽, 아래, 오른대각,왼대각 만 찾자
+# 찾은 o를 방향 벡터로 움직이면서 연속되는 오목알 5개를 찾으면 YES 후 브레이크
+# 끝까지 못찾으면 NO
 
-# ---------------------------------------
-# bit 연산 활용
-bin_str = []
-for val in arr:
-    num = int(val, 16)
-    bin_str.append(1 if num & 8 else 0)
-    bin_str.append(1 if num & 4 else 0)
-    bin_str.append(1 if num & 2 else 0)
-    bin_str.append(1 if num & 1 else 0)
-print(bin_str)
+import sys
+# sys.stdin = open('swea\\input\\11315.txt')
+sys.stdin = open('al_sloving\\swea\\input\\11315.txt')
+sys.setrecursionlimit(10 ** 6)
+
+# 방향 벡터 설정
+di = [0, 1, 1, 1] # 우 하 우하 좌하
+dj = [1, 0, 1, -1]
+
+def omok(arr):
+    global cnt
+    
+    for i in range(N):
+        for j in range(N):
+
+            if arr[i][j] == 'o':
+                for k in range(4): # 바로 돌지 않음
+                    ni = i
+                    nj = j
+                    cnt = 0
+                    # 연속?
+                    while 0 <= ni < N and 0 <= nj < N and arr[ni][nj] == 'o':
+                        cnt += 1
+                        ni += di[k] # while문에서 한방향으로 계속 돌기
+                        nj += dj[k]
+                    if cnt >= 5:
+                        return 'YES'
+    return 'NO'
+
+T = int(input())
+for tc in range(1, T+1):
+    N = int(input())
+    arr = [input() for _ in range(N)]
+
+    print(f'#{tc} {omok(arr)}')
--- Swea/D3/11315.오목 판정.py
+++ Swea/D3/11315.오목 판정.py
@@ -1,45 +1,45 @@
+# 11315. 오목
+
+# 구현으로 풀어보자
+# 주어진 정수 N 은 오목판의 크기 (가로x세로)
+# 문자열로 배열 만들기
+# 포문 두개로 배열 돌면서 o 찾기
+# 방향 벡터는 오른쪽, 아래, 오른대각,왼대각 만 찾자
+# 찾은 o를 방향 벡터로 움직이면서 연속되는 오목알 5개를 찾으면 YES 후 브레이크
+# 끝까지 못찾으면 NO
+
 import sys
 # sys.stdin = open('swea\\input\\11315.txt')
 sys.stdin = open('al_sloving\\swea\\input\\11315.txt')
+sys.setrecursionlimit(10 ** 6)
+
+# 방향 벡터 설정
+di = [0, 1, 1, 1] # 우 하 우하 좌하
+dj = [1, 0, 1, -1]
+
+def omok(arr):
+    global cnt
+    
+    for i in range(N):
+        for j in range(N):
+
+            if arr[i][j] == 'o':
+                for k in range(4): # 바로 돌지 않음
+                    ni = i
+                    nj = j
+                    cnt = 0
+                    # 연속?
+                    while 0 <= ni < N and 0 <= nj < N and arr[ni][nj] == 'o':
+                        cnt += 1
+                        ni += di[k] # while문에서 한방향으로 계속 돌기
+                        nj += dj[k]
+                    if cnt >= 5:
+                        return 'YES'
+    return 'NO'
 
 T = int(input())
 for tc in range(1, T+1):
     N = int(input())
     arr = [input() for _ in range(N)]
 
-    result = 'NO'
-    if result == 'NO':
-        for i in range(N): # 세로
-            for q in range(N):
-                if arr[i][q] == 'o':
-                    continue
-                else:
-                    break
-            result = 'YES'
-
-    elif result == 'NO':        
-        for j in range(N): # 가로
-            for w in range(N):
-                if arr[w][j] == 'o':
-                    continue
-                else:
-                    break
-            result = 'YES'
-    
-    elif result == 'NO': # 대각선
-        for k in range(N):
-            if arr[k][k] == 'o':
-                continue
-            else:
-                break
-        result = 'YES'
-
-    elif result == 'NO': # 역대각선
-        for l in range(N):
-            if arr[N-1-l][l] == 'o':
-                continue
-            else:
-                break
-        result = 'YES'
-
-    print(f'#{tc} {result}')
+    print(f'#{tc} {omok(arr)}')
