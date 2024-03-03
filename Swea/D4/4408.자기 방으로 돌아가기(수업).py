50dcdadd22b6b6f42ca39bc1c0a11d50892f5506
Jinwoo <yasisicoco@gmail.com>
2024-03-04 01:56:45 +0900

    240303_beforeIM

--- Swea/D4/4408.자기 방으로 돌아가기(수업).py
+++ Swea/D4/4408.자기 방으로 돌아가기(수업).py
@@ -21,3 +21,28 @@ for tc in range(1, T+1):
             corridor[i] += 1
             max_v = max(corridor[i], max_v)
     print(f'#{tc} {max_v}')
+    
+    
+# state 2
+T = int(input())
+for tc in range(1, T+1):
+    N = int(input())
+    path = [0] * 400 # 통로
+    
+    for i in range(N):
+        s, e = map(int, input().split())
+        
+        if s > e:
+            s, e = e, s
+        
+        # 한줄의 방 배열로 압축
+        if s % 2 == 0:
+            s -= 1
+        if e % 2 == 0:
+            e -= 1
+            
+        for j in range(s, e+1):
+            path[j] += 1
+        
+        max_val = max(path)
+    print(f'#{tc} {max_val}')
