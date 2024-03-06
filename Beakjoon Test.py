d75b45283e68d148c1b29f73d983d2cda8ba3579
김도이 <kimdoiary@gmail.com>
2024-03-06 17:55:58 +0900

    al

--- Beakjoon Test.py
+++ Beakjoon Test.py
@@ -1,41 +1,25 @@
-from collections import deque
+def recur(cur):
+    if len(password) == 4:
+        for k in password:
+            if k in 'aeiou':
+                password.sort()
+                print(*password, sep='')
+            # break    
+        return
+    
+    for i in range(cur, C):
+        if visited[i]:
+            continue
+        visited[i] = True
+        password.append(str_lst[i])
+        recur(i)
+        password.pop()
+        visited[i] = False
 
-def bfs(cur):
-    cnt = 0
 
-    Q = deque()
-    Q.append(cur) # 현재위치 넣기
-
-    while Q:
-        cur = Q.popleft()
-        for i in v[cur]:
-            if visited[i]:
-                continue
-            visited[i] = True
-            cnt += 1
-            if i == insrt2:
-                return cnt
-            Q.append(i)
-    return -1
-
-n = int(input()) # 전체 사람의 수 (정점)
-p1, p2 = map(int, input().split()) # 비교해야 하는 두 사람
-m = int(input()) # 부모 자식들간의 관계의 개수 (간선)
-
-v = [[] for _ in range(n+1)] # 인접정점
-visited = [False] * (n+1)
-
-for i in range(m):
-    a, b = map(int, input().split())
-    v[a].append(b)
-    v[b].append(a)
-
-# print(v)
-if p1 < p2:
-    insrt = p1
-    insrt2 = p2
-else:
-    insrt = p2
-    insrt2 = p1
-visited[insrt] = True
-print(bfs(insrt))
+L, C = map(int, input().split())
+str_lst = list(input().split())
+str_lst.sort()
+visited = [False] * (C+1)
+password = []
+recur(0)
---
+++ test.py
@@ -0,0 +1,8 @@
+while True:
+    try:
+        N = int(input())
+        st = ['-'] * (3**N)
+        print(st)
+        
+    except:
+        break
