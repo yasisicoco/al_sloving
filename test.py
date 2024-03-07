25be14f297f7b8f65d66ffa392e3c1b96a88458e
jinwoo <yasisicoco@gmail.com>
2024-03-07 23:53:04 +0900

    lose

--- test.py
+++ test.py
@@ -1,12 +1,25 @@
-while True:
-    try:
-        N = int(input())
-        st = ['-'] * (3**N)
-        num = len(st)+1 // 3
-        for i in range(1, (len(st)+1)):
-            if i == num * 1: # 첫번째 지우는 라인
-                pass
-            elif i == num * 2: # 여기까지 지움
-                st[i-1:num*2-1] = ' ' # 공백으로채움
-    except: 
-        break
+N = int(input())
+
+lst = []
+for m in range(N):
+    t, p = map(int, input().split())
+    lst.append((t, p))
+
+# print(lst)
+
+result = 0
+for i in range(N-1):
+    day = lst[i][0] # 0: 일수, 1: 수입
+    sumone = lst[i][1]
+    for j in range(day+1, N):
+        day += lst[j][0] # 0: 일수, 1: 수입
+        sumone += lst[j][1]
+        if day > N: # 근무일수가 지났으면
+            day -= lst[j][0] # 전 근무일수 빼고 다시진행
+            sumone -= lst[j][1]
+            continue
+        else: # 근무일수가 안지났으면 지금까지 더한 수익을 result와 비교 후 갱신
+            if result <= sumone:
+                result = sumone
+
+print(result)
