20abb07b1b6a97a7b5cc24885a3b5f0abef2e996
Jinwoo <yasisicoco@gmail.com>
2024-03-12 01:24:42 +0900

    0312

--- test.py
+++ test.py
@@ -8,8 +8,10 @@
 4. 카운트한 안전영역이 최대값이면 갱신
 '''
 import sys
+from collections import deque
 input = sys.stdin.readline
 sys.setrecursionlimit(10**6)
+
 
 # 방향 벡터
 di = [0, 1, 0, -1] # 우 하 좌 상
@@ -22,54 +24,65 @@ def virus(vi, vj):
         ni = vi + di[k]
         nj = vj + dj[k]
 
-        if 0 <= ni < N and 0 <= nj < M and not visited_virus[ni][nj] and arr_2[ni][nj] == 0:
+        if 0 <= ni < N and 0 <= nj < M and not visited_virus[ni][nj] and arr[ni][nj] == 0:
             visited_virus[ni][nj] = True
-            arr_2[ni][nj] = 2
+            arr[ni][nj] = 2
             virus(ni, nj)
 
 # 안전지대의 수 세기
-def safe(arr_2):
+def safe(arr):
     safeZone = 0
     for i in range(N):
         for j in range(M):
-            if arr_2[i][j] == 0:
+            if arr[i][j] == 0:
                 safeZone += 1
     return safeZone
 
 # 안전지대 벽세우기
-def wall(arr_2):
-    cnt = 0 # 벽의 갯수
-    for i in range(N):
-        for j in range(M):
-            if arr_2[i][j] == 0:
-                arr_2[i][j] = 1
-                cnt += 1
-            if cnt == 3:
-                break
-        if cnt == 3:
-            break
-    return arr_2
+def wall(start, cnt, num, arr):
+    global zero, result, visited_recur
+    if cnt == 3: # 벽 세개 세웠을 때
+        # 재귀로 돌면서 세개 1로 바꿨을 때, 바이러스 유포
+        for i in range(N):
+            for j in range(M):
+                if arr[i][j] == 2:
+                    vi, vj = i, j # 바이러스 시작좌표
+                    virus(vi, vj)
+        ans = safe(arr) # 갱신코드
+        if result <= ans:
+            result = ans
+        return 
+            
+    for i in range(start, num):
+        if visited_recur[i] == True:
+            continue
+        a, b = zero[i]
+        visited_recur[i] = True
+        arr[a][b] = 1
+        wall(start+1, cnt+1, num, arr)
+        visited_recur = False
+        arr[a][b] = 0
 
+        
+        
+#---------------------------------------------------------------------
 # 세로 크기 N, 가로 크기 M
 N, M = map(int, input().split())
 # 연구소 지도
-arr = [list(map(int, input().split())) for _ in range(N)] 
+arr = [list(map(int, input().split())) for _ in range(N)]
+visited_virus = [[False] * M for _ in range(N)] # 바이러스용 방문표시
+arr_2 = arr[:]
+zero = []
+# 0인 좌표를 리스트에 넣기
+for i in range(N):
+    for j in range(M):
+        if arr[i][j] == 0:
+            zero.append((i, j))
 
-result = 0 # 안전지대 검사
-while True:
-    visited_virus = [[False] * M for _ in range(N)] # 바이러스용 방문표시
-    # 벽을 3개 세울 0(안전지대) 찾아서 세우기
-    arr_2 = wall(arr)
+result = 0
+zero = deque(zero) # 재귀 시작
+num = len(zero)
+visited_recur = [False] * (num+10) # 재귀용 방문표시
+wall(0, 0, num, arr)
 
-    # 바이러스 찾아서 퍼뜨리기
-    for i in range(N):
-        for j in range(M):
-            if arr_2[i][j] == 2:
-                vi, vj = i, j # 바이러스 시작좌표
-                virus(vi, vj)
-    
-    ans = safe(arr_2)
-    if result <= ans:
-        result = ans
-    
-print(ans)
+print(result)
