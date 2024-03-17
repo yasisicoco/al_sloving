11a6d7634c6421d3361eb2d426bad6fe6f2efd43
MooYoo <82500842+yasisicoco@users.noreply.github.com>
2024-03-17 10:58:34 +0900

    [Silver III] Title: 도시와 비트코인, Time: 188 ms, Memory: 31988 KB -BaekjoonHub

--- 백준/Silver/31575. 도시와 비트코인/도시와 비트코인.py
+++ 백준/Silver/31575. 도시와 비트코인/도시와 비트코인.py
@@ -1,26 +1,24 @@
-di = [0, 1] # 오른쪽 아래쪽
-dj = [1, 0]
+def dfs(x,y):
+    global ans
+    # visited[x][y] = 1
 
-def dfs(si, sj):
-    global result 
-    
-    if si == M-1 and sj == N-1:
-        result = 'Yes'
+    if x == m - 1 and y == n - 1:
+        ans = 'Yes'
         return
-        
-    
-    for k in range(2):
-        ni = si + di[k]
-        nj = sj + dj[k]
 
-        if 0 <= ni < M and 0 <= nj < N and not visited[ni][nj] and arr[ni][nj] != 0:
-            visited[ni][nj] = True
-            dfs(ni, nj)            
-        
-N, M = map(int, input().split())
-arr = [list(map(int, input().split())) for _ in range(M)]
-visited = [[False] * N for _ in range(M)]
-visited[0][0]=True
-result = 'No'
-dfs(0,0)
-print(result)
+    for k in range(2):
+        ni = x + di[k]
+        nj = y + dj[k]
+        if 0 <= ni < m and 0 <= nj < n and arr[ni][nj] != 0 and visited[ni][nj] == 0:
+            visited[ni][nj] = 1
+            dfs(ni, nj)
+
+n, m = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(m)]
+visited = [[0]*n for _ in range(m)]
+di = [0, 1]  # 우,하 방향설정
+dj = [1, 0]
+ans = 'No'
+
+dfs(0, 0)
+print(ans)
