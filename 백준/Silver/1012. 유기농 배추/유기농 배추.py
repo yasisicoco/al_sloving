528f2a810a0a76c317afd9f63af592df76c2547d
MooYoo <82500842+yasisicoco@users.noreply.github.com>
2024-04-26 22:29:36 +0900

    [Silver II] Title: 유기농 배추, Time: 368 ms, Memory: 34096 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,36 +1,36 @@
-import sys
-sys.setrecursionlimit(10**6)
+from collections import deque
 
-def dus(i, j):
-    
-    for dir in range(4):
-        ni = i + di[dir]
-        nj = j + dj[dir]
-
-        if 0 <= ni < N and 0 <= nj < M and arr[ni][nj] == 1 and not visited[ni][nj]:
-            visited[ni][nj] = True
-            dus(ni, nj)
-
-di = [0, 1, -1, 0]
-dj = [1, 0, 0, -1]
+def bfs(si, sj):
+    di = [0, 1, -1, 0]
+    dj = [1, 0, 0, -1]
+    q = deque()
+    q.append([si, sj])
+    visited[si][sj] = True
+    while q:
+        i, j = q.popleft()
+        for k in range(4):
+            ni = i + di[k]
+            nj = j + dj[k]
+            if 0 <= ni < N and 0 <= nj < M and not visited[ni][nj] and arr[ni][nj] == 1:
+                visited[ni][nj] = True # 방문처리
+                q.append([ni, nj])
 
 T = int(input())
-for tc in range(1, T+1):
-    M, N, K = map(int, input().split())
-    
-    arr = [[0 for _ in range(M)] for _ in range(N)]
-    visited = [[False for _ in range(M)] for _ in range(N)]
-    
+for tc in range(T):
+    N, M, K = map(int, input().split())
+
+    # 배추맵
+    arr = [[0] * M for _ in range(N)]
     for _ in range(K):
-        a, b = map(int, input().split())
-        arr[b][a] = 1
-    
-    cnt = 0 
-    for l in range(N): # 그래프탐색
-        for k in range(M):
-            if arr[l][k] == 1 and not visited[l][k]: # 1이고 방문하지 않은곳
-                visited[l][k] = True
-                cnt += 1  # 카운트 +1
-                dus(l, k) # 재귀로 주변 1을 돌면서 전부 방문처리
-                
-    print(cnt)
+        r, c = map(int, input().split())
+        arr[r][c] = 1
+    # 배추방문맵
+    visited = [[False] * M for _ in range(N)]
+
+    cabage = 0
+    for i in range(N):
+        for j in range(M):
+            if arr[i][j] == 1 and not visited[i][j]:
+                bfs(i, j)
+                cabage += 1
+    print(cabage)
