a1c256d580617b341a9cdc489a4a5041d41efda6
MooYoo <82500842+yasisicoco@users.noreply.github.com>
2024-06-17 20:49:17 +0900

    [Bronze V] Title: 사칙연산, Time: 108 ms, Memory: 9608 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.js
@@ -0,0 +1,12 @@
+const fs = require("fs")
+// let input = fs.readFileSync("./input.txt").toString().split(' ').map(Number)
+
+let input = fs.readFileSync("/dev/stdin").toString().split(' ').map(Number)
+
+const a = input[0]
+const b = input[1]
+console.log(a + b)
+console.log(a - b)
+console.log(a * b)
+console.log(Math.floor(a / b))
+console.log(a % b)
