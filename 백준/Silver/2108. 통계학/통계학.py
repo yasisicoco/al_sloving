3bde76ba1d2f9de2bfbef4566e1c4805e81856b8
MooYoo <82500842+yasisicoco@users.noreply.github.com>
2024-07-04 23:58:13 +0900

    [Silver III] Title: 통계학, Time: 492 ms, Memory: 52832 KB -BaekjoonHub

---
+++ 백준/Silver/2108. 통계학/통계학.py
@@ -0,0 +1,33 @@
+from collections import Counter
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+arr = []
+for i in range(N):
+    arr.append(int(input()))
+
+# 산술 평균
+a = round(sum(arr) / N)
+
+# 중앙값
+arr.sort()
+if N % 2 == 0:
+    b = (arr[N//2 - 1] + arr[N//2]) / 2
+else:
+    b = arr[N//2]
+
+# 최빈값
+freq = Counter(arr).most_common()
+if len(freq) > 1 and freq[0][1] == freq[1][1]:
+    c = freq[1][0]
+else:
+    c = freq[0][0]
+
+# 범위
+d = max(arr) - min(arr)
+
+print(a)
+print(b)
+print(c)
+print(d)
