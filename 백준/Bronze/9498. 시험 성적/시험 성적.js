1befaa754b351a66747c44fbcc2e88bccd9ec5c7
MooYoo <82500842+yasisicoco@users.noreply.github.com>
2024-12-29 16:46:54 +0900

    [Bronze V] Title: 시험 성적, Time: 100 ms, Memory: 9340 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.js
+++ 백준/Bronze/9498. 시험 성적/시험 성적.js
@@ -1,16 +1,14 @@
-const fs = require("fs")
-// let input = fs.readFileSync("./input.txt").toString().trim()
-let input = fs.readFileSync("/dev/stdin").toString().trim()
-const a = parseInt(input, 10)
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().split(" ").map(Number);
 
-if (a >= 90) {
-  console.log("A")
-} else if (a >= 80) {
-  console.log("B")
-} else if (a >= 70) {
-  console.log("C")
-} else if (a >= 60) {
-  console.log("D")
+if (input >= 90) {
+  console.log("A");
+} else if (input >= 80) {
+  console.log("B");
+} else if (input >= 70) {
+  console.log("C");
+} else if (input >= 60) {
+  console.log("D");
 } else {
-  console.log("F")
+  console.log("F");
 }
