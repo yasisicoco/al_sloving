3971344ad10809f4abc4b7e27db8f4b5237d4f90
MooYoo <82500842+yasisicoco@users.noreply.github.com>
2024-12-30 00:18:30 +0900

    [Bronze III] Title: 알람 시계, Time: 104 ms, Memory: 9340 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.js
+++ 백준/Bronze/2884. 알람 시계/알람 시계.js
@@ -1,18 +1,21 @@
 const fs = require("fs");
-// let input = fs.readFileSync("./input.txt").toString().trim().split(" ");
-let input = fs.readFileSync("/dev/stdin").toString().trim().split(" ");
-let H = parseInt(input[0], 10);
-let M = parseInt(input[1], 10);
+let [hour, minute] = fs
+  .readFileSync("/dev/stdin")
+  .toString()
+  .split(" ")
+  .map(Number);
 
-if (0 <= H && H <= 23 && 0 <= M && M <= 59) {
-  if (M < 45) {
-    H = H - 1;
-    M = M + 15;
-    if (H < 0) {
-      H = H + 24;
+if (0 <= hour <= 23 && 0 <= minute <= 59) {
+  if (minute < 45) {
+    hour -= 1;
+    minute += 15;
+    if (hour < 0) {
+      hour += 24;
     }
   } else {
-    M = M - 45;
+    minute -= 45;
   }
-  console.log(H, M);
+}
+{
+  console.log(hour, minute);
 }
