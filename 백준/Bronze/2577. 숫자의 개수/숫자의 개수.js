a9551a81cb606abbe9da4ffab07caa70967db051
MooYoo <82500842+yasisicoco@users.noreply.github.com>
2024-12-30 14:01:45 +0900

    [Bronze II] Title: 숫자의 개수, Time: 92 ms, Memory: 9572 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.js
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.js
@@ -5,16 +5,10 @@ const [A, B, C] = fs
   .split("\n")
   .map(Number);
 
-const num = A * B * C;
-const num_str = num.toString();
+const num = (A * B * C).toString();
 let array = new Array(10).fill(0);
-for (let i = 0; i < 10; i++) {
-  for (let j = 0; j < num_str.length; j++) {
-    const str = Number(num_str[j]);
-    if (str === i) {
-      array[i] += 1;
-    }
-  }
+for (let count of num) {
+  array[Number(count)]++;
 }
 
-array.forEach((item) => console.log(item));
+console.log(array.join("\n"));
