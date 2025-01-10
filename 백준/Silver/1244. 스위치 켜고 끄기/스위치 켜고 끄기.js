0ab6d222a9fe3e67cc5059d88daba3fe2e8cf308
MooYoo <82500842+yasisicoco@users.noreply.github.com>
2025-01-10 12:53:58 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 92 ms, Memory: 9356 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.js
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.js
@@ -10,24 +10,37 @@ for (let i = 3; i < stuNum + 3; i++) {
 
   // 남학생1 = 스위치번호가 자기가 받은 수의 배수이면, 스위치의 상태바꿈
   if (student[0] === 1) {
-    for (let j = 1; j * student[1] <= array.length; j++) {
-      const boy = j * student[1] - 1;
-      array[boy] = 1 - array[boy];
+    for (let j = 0; j < 101; j++) {
+      const boy = student[1] * j - 1;
+      if (array[boy] === 1 && boy < array.length) {
+        array[boy] = 0;
+      } else if (array[boy] === 0 && boy <= array.length) {
+        array[boy] = 1;
+      }
     }
   } else {
     // 여학생2 = 스위치번호를 중심으로 대칭되는 스위치가 다를때까지 바꿈
     const girl = student[1] - 1;
-    array[girl] = 1 - array[girl];
-    for (let k = 1; k < array.length; k++) {
+    if (array[girl] === 0) {
+      array[girl] = 1;
+    } else {
+      array[girl] = 0;
+    }
+    for (let k = 1; k < 101; k++) {
       const girlPrev = girl - k;
       const girlNext = girl + k;
       if (
         array[girlPrev] === array[girlNext] &&
         girlPrev >= 0 &&
-        girlNext < array.length
+        girlNext <= 100
       ) {
-        array[girlPrev] = 1 - array[girlPrev];
-        array[girlNext] = 1 - array[girlNext];
+        if (array[girlPrev] === 1) {
+          array[girlPrev] = 0;
+          array[girlNext] = 0;
+        } else {
+          array[girlPrev] = 1;
+          array[girlNext] = 1;
+        }
       } else {
         break;
       }
