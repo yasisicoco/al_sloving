52e3d8a03d97ff6ee0e141580ae852b5a1acc201
MooYoo <82500842+yasisicoco@users.noreply.github.com>
2025-02-10 12:34:22 +0900

    [level 2] Title: 숫자 변환하기, Time: 689.90 ms, Memory: 17.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
@@ -0,0 +1,17 @@
+def solution(x, y, n):
+    
+    if x >= y:
+        return 0 if x == y else -1
+    
+    dp = [float('inf')] * (y+1)
+    dp[x] = 0
+    
+    for i in range(x, y+1):
+        if i + n <= y:
+            dp[i+n] = min(dp[i+n], dp[i]+1)
+        if i * 2 <= y:
+            dp[i*2] = min(dp[i*2], dp[i]+1)
+        if i * 3 <= y:
+            dp[i*3] = min(dp[i*3], dp[i]+1)
+    
+    return dp[y] if dp[y] != float('inf') else -1
