4ca376ec357395bbffeec984e8d66a7c774a2cc2
MooYoo <82500842+yasisicoco@users.noreply.github.com>
2025-03-13 03:37:50 +0900

    [Silver V] Title: 다리 놓기, Time: 84 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.py
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.py
@@ -3,15 +3,14 @@ for _ in range(tc):
     N, M = map(int, input().split())
     arr = [[0] * (M+1) for _ in range(N+1)]
 
-    for i in range(N+1):
+    for j in range(M+1):
+        arr[1][j] = j
+        
+    for i in range(2, N+1):
+        prev = [0] * (M+1)
         for j in range(M+1):
-            if i == 0:
-                arr[i][j] = 0
-            elif i == 1:
-                arr[i][j] = j
-            elif i == j:
-                arr[i][j] = 1
-            else:
-                arr[i][j] = sum(arr[i-1][1:j])
+            prev[j] = prev[j-1] + arr[i-1][j]
+        for j in range(i, M+1):
+            arr[i][j] = prev[j-1]
 
     print(arr[N][M])
