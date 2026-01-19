164bf1d0c401f028f4708de10f1791ae19ff93dd
MooYoo <82500842+yasisicoco@users.noreply.github.com>
2026-01-19 22:38:12 +0900

    [Silver III] Title: N과 M (1), Time: 168 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,18 +1,21 @@
+import sys
+input = sys.stdin.readline
+
 N, M = map(int, input().split())
-visited = [False for _ in range(N+10)]
+visited = [False for _ in range(N+1)]
 arr = []
 
 def back(N, M):
     if len(arr) == M:
         print(*arr)
         return
+    
     for i in range(1, N+1):
-        if visited[i]:
-            continue
-        visited[i] = True
-        arr.append(i)
-        back(N, M)
-        arr.pop()
-        visited[i] = False
+        if not visited[i]:
+            visited[i] = True
+            arr.append(i)
+            back(N, M)
+            arr.pop()
+            visited[i] = False
 
 back(N, M)
