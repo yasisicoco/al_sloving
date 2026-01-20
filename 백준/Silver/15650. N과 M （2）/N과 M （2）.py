78ae21a0a179b8218e571255ae5b8e81e476f51f
MooYoo <82500842+yasisicoco@users.noreply.github.com>
2026-01-20 23:38:13 +0900

    [Silver III] Title: N과 M (2), Time: 76 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,33 +1,25 @@
-# N까지의 자연수, 중복없이 M개
 N, M = map(int, input().split())
-# 부분집합용 리스트
-arr = []
-# 중복방지용 방문 (자연수가1부터니까 인덱스 +1)
-visited = [False for _ in range(N+1)]
 
-def recur(cur):
-    global ans
-
-    if len(arr) == M: # 부분집합길이가 M이 되면
-        # sort?
-        ret = arr[:]
-        ret.sort()
-        if ret not in ans:
-            ans.append(ret)
-
-        # print(*arr)
+def back():
+    if len(arr) == M:
+        temp = sorted(arr)
+        if temp not in results:
+            results.append(temp)
         return
-
-    for i in range(1, N+1): # 1부터 N까지의 자연수
-        if visited[i]: # True
+    
+    for i in range(1, N+1):
+        if visited[i]:
             continue
         visited[i] = True
-        arr.append(i) # 부분집합 리스트로 추가
-        recur(cur+1)
+        arr.append(i)
+        back()
         arr.pop()
         visited[i] = False
+        
 
-ans = []
-recur(1)
-for line in ans:
-    print(*line)
+arr = []
+visited = [False for _ in range(N+1)]
+results = []
+back()
+for result in results:
+    print(*result)
