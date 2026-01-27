559c6a7045b3723c26e55f04c8341c12dbeb6fd1
MooYoo <82500842+yasisicoco@users.noreply.github.com>
2026-01-27 23:42:25 +0900

    [Silver II] Title: N과 M (10), Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15664. N과 M （10）/N과 M （10）.py
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.py
@@ -8,16 +8,13 @@ def back(s):
         return
     
     for i in range(s, N):
-        if not visited[i] and check != lst[i]:
+        if check != lst[i]:
             check = lst[i]
-            visited[i] = True
             arr.append(lst[i])
             back(i+1)
             arr.pop()
-            visited[i] = False
 
 lst.sort()
 arr = []
-visited = [False] * (N+1)
 back(0)
 # 1 7 9 9 
